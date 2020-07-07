48cfaaa7ae631944a6e063221f7658f2986d9f7b
liupengfei <liupengfei@huiyihuiying.com>
2020-07-07 17:54:12 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+**umtk** is an open source python library for medical image processing.
+
+## Installation
+
+*Install umtk*
+
+```shell
+$ git clone git@github.com:kyle0x54/umtk.git
+$ cd umtk
+$ pip install -e .
+```
+
+## Try your first umtk program
+
+```python
+>>> import umtk
+>>> umtk.isdicom('001.dcm')
+True
+```
+
+## License
+
+[Apache License 2.0](LICENSE)
---
+++ setup.py
@@ -0,0 +1,68 @@
+import os
+from setuptools import find_packages, setup
+
+
+NAME = 'umtk'
+DESCRIPTION = 'A python library for medical image processing.'
+URL = 'https://github.com/kyle0x54/umtk'
+EMAIL = 'kyle0x54@163.com'
+AUTHOR = 'kyle0x54'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+
+REQUIRED = [
+    'matplotlib',
+    'natsort',
+    'numpy',
+    'opencv-python',
+    'pandas',
+    'pydicom',
+    'SimpleITK',
+    'tqdm',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with open(os.path.join(here, 'README.md')) as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='APACHE',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    zip_safe=False,
+)
