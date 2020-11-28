29bf820aaf9bd45dc94974f93e9c727e88b8b3b5
kyle0x54 <kyle0x54@163.com>
2020-11-28 14:21:05 +0800

    fix: fix remove_small_hole bug

--- umtk/cupy_utils/morphology/misc.py
+++ umtk/cupy_utils/morphology/misc.py
@@ -1,4 +1,12 @@
 import cupy
+from cupyx.scipy.ndimage import label
+
+
+def _check_dtype_supported(ar):
+    if not (ar.dtype == bool or cupy.issubdtype(ar.dtype, cupy.integer)):
+        raise TypeError(
+            "Only bool or integer types are supported. Got %s." % ar.dtype
+        )
 
 
 def remove_small_objects_gpu(
@@ -8,12 +16,16 @@ def remove_small_objects_gpu(
     """ See scikit-image remove_small_objects()
 
     N.B.
-        Input array must be a labeled mask.
+        Input array can be a binary mask (bool type) or
+        labeled mask (int type).
         This is a inplace operation.
     """
-    component_sizes = cupy.bincount(mask.ravel())
+    _check_dtype_supported(mask)
+
+    ccs = label(mask) if mask.dtype == bool else mask
+    component_sizes = cupy.bincount(ccs.ravel())
     too_small = component_sizes < min_size
-    too_small_mask = too_small[mask]
+    too_small_mask = too_small[ccs]
     mask[too_small_mask] = 0
 
 
@@ -24,9 +36,12 @@ def remove_small_holes_gpu(
     """ See scikit-image remove_small_holes()
 
     N.B.
-        Input array must be a labeled mask.
+        Input array must be a binary mask (bool type) or
+        labeled mask (int type).
         This is a inplace operation.
     """
+    _check_dtype_supported(mask)
+
     cupy.logical_not(mask, out=mask)
     remove_small_objects_gpu(mask, area_threshold)
     cupy.logical_not(mask, out=mask)
