746ed8421b0558447cd01756388153fa1277b07e
kyle0x54 <kyle0x54@163.com>
2020-11-28 11:17:43 +0800

    feat: support remove small holes

--- umtk/cupy_utils/morphology/__init__.py
+++ umtk/cupy_utils/morphology/__init__.py
@@ -1,5 +1,8 @@
 # flake8: noqa
 
-from .misc import remove_small_objects_gpu
+from .misc import (
+    remove_small_objects_gpu,
+    remove_small_holes_gpu
+)
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
--- umtk/cupy_utils/morphology/misc.py
+++ umtk/cupy_utils/morphology/misc.py
@@ -15,3 +15,18 @@ def remove_small_objects_gpu(
     too_small = component_sizes < min_size
     too_small_mask = too_small[mask]
     mask[too_small_mask] = 0
+
+
+def remove_small_holes_gpu(
+    mask: cupy.ndarray,
+    area_threshold: int,
+) -> None:
+    """ See scikit-image remove_small_holes()
+
+    N.B.
+        Input array must be a labeled mask.
+        This is a inplace operation.
+    """
+    cupy.logical_not(mask, out=mask)
+    remove_small_objects_gpu(mask, area_threshold)
+    cupy.logical_not(mask, out=mask)
