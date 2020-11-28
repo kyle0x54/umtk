29aaaff4049a29b21056751b97e8b6f8f1066f56
kyle0x54 <kyle0x54@163.com>
2020-11-28 11:09:36 +0800

    feat: support cupy->tensor, cupy\tensor->numpy

--- umtk/cupy_utils/__init__.py
+++ umtk/cupy_utils/__init__.py
@@ -1,6 +1,11 @@
 # flake8: noqa
 
-from .convert import tensor2cupy_gpu
+from .convert import (
+    tensor2cupy_gpu,
+    cupy2tensor_gpu,
+    tensor2numpy,
+    cupy2numpy
+)
 from .morphology import remove_small_objects_gpu
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
--- umtk/cupy_utils/convert.py
+++ umtk/cupy_utils/convert.py
@@ -1,12 +1,27 @@
 import cupy
+from cupy.core.dlpack import toDlpack, fromDlpack
+import numpy as np
 import torch
-from torch.utils.dlpack import to_dlpack
+from torch.utils.dlpack import to_dlpack, from_dlpack
 
 
-def tensor2cupy_gpu(
-        torch_tensor: torch.Tensor
-) -> cupy.ndarray:
-    """ Convert a pytorch tensor to cupy_utils array."""
+def tensor2cupy_gpu(torch_tensor: torch.Tensor) -> cupy.ndarray:
+    """ Convert a pytorch tensor to cupy array."""
     with cupy.cuda.Device(torch_tensor.device.index):
-        cupy_array = cupy.fromDlpack(to_dlpack(torch_tensor))
+        cupy_array = fromDlpack(to_dlpack(torch_tensor))
         return cupy_array
+
+
+def cupy2tensor_gpu(cupy_array: cupy.ndarray) -> torch.Tensor:
+    """ Convert a cupy array to pytorch tensor."""
+    return from_dlpack(toDlpack(cupy_array))
+
+
+def tensor2numpy(torch_tensor: torch.Tensor) -> np.ndarray:
+    """ Convert a pytorch tensor to numpy array."""
+    return torch_tensor.cpu().numpy()
+
+
+def cupy2numpy(cupy_array: cupy.ndarray) -> np.ndarray:
+    """ Convert a cupy array to numpy array."""
+    return cupy.asnumpy(cupy_array)
